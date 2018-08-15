eb7b3141d4e1cb77671871116c373d2b07e329e9
wambui7 <shiko.sheila@gmail.com>
2018-08-14 20:21:22 -0500

    no changes

--- dates/Program.cs
+++ dates/Program.cs
@@ -6,7 +6,12 @@ namespace dates
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DateTime startdate = DateTime.Now;
+            DateTime enddate = Convert.ToDateTime("12, 31, 2018");
+            TimeSpan date = (enddate - startdate);
+            Console.WriteLine(date.TotalDays);
+            Console.ReadLine();
+
         }
     }
 }
