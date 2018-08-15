636a7c0d081e05aa15056088addda85752c58cdd
wambui7 <shiko.sheila@gmail.com>
2018-08-14 19:35:02 -0500

    Add project files.

---
+++ dates/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
